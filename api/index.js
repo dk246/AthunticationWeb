a06e2c075fb8744c9c42c33b46f112faa57a5385
dk246 <76243568+dk246@users.noreply.github.com>
2024-06-12 23:58:33 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://dumindu:dumindu24@mern.8sp1ced.mongodb.net/?retryWrites=true&w=majority&appName=mern"
+  )
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
