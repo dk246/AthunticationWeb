fb124bdd461db0a409ce9e7de12cab439894aa59
dk246 <76243568+dk246@users.noreply.github.com>
2024-06-29 10:10:45 +0530

    sign in functionality completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
 
@@ -9,6 +10,23 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created success" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return console.log("user not found");
+
+    const validPass = await bcryptjs.compareSync(password, validUser.password);
+    if (!validPass) return console.log("password is not valid");
+
+    const token = jwt.sign({ id: validUser._id }, "ttt");
+    res.cookie("acces_token", token, { httpOnly: true }).json(validUser);
   } catch (error) {
     next(error);
   }
